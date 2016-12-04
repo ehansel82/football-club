474ca5faf06f1c15115fdcff57748f8b226e4880
Eric Hansel <e.hansel.01@gmail.com>
2016-12-04 09:26:35 -0500

    Commit merge

--- app/models/game.ts
+++ app/models/game.ts
@@ -1,5 +1,4 @@
 class Game {
-<<<<<<< HEAD
     groupID: string;
     date: string;
     qb: boolean;
@@ -7,7 +6,4 @@ class Game {
     team2: Player[];
     team1Score: number;
     team2Score: number;
-=======
-
->>>>>>> 814187084d391c8ff28451c22a3b5cc59079dd1e
 }
